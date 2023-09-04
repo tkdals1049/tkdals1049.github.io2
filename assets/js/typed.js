6f5573316f445b6642c1937f140719805b7c7599
tkdals1049 <tkdals1049@naver.com>
2023-09-05 04:25:43 +0900

    Update typed.js

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -3,7 +3,22 @@ import { Typewriter } from 'react-simple-typewriter'
 
 $('#subtitle').html('test2'+'<span></span>');
 var tes= ["</br>Introduction:<br/>"+"site.tagline "];
-
+const words = ["해적왕이야", "멋진 개발자가 될 거야"];
+const { text } = useTypewriter({
+    words,
+    loop: 0, 
+    cursorStyle: '|',
+});
+<Contain>
+        <Text>
+            나는
+            <span>
+                {text}
+            </span>
+            <Cursor />
+            <h5>velog.io</h5>
+        </Text>
+    </Contain>
   const typed2 = new useTypewriter('#subtitle span', {
   strings: tes,
   typeSpeed: 60,
