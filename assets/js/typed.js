6fe6ee4e15efcc64f41e944266f965f892f0d63d
tkdals1049 <tkdals1049@naver.com>
2023-09-05 04:25:04 +0900

    Update typed.js

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import { Typewriter } from 'react-simple-typewriter'
+
 $('#subtitle').html('test2'+'<span></span>');
 var tes= ["</br>Introduction:<br/>"+"site.tagline "];
 
