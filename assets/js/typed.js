d9611df70b869d78a78216e0442cc47192af4e30
tkdals1049 <tkdals1049@naver.com>
2023-09-05 04:26:03 +0900

    Update typed.js

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Typewriter } from 'react-simple-typewriter'
 
-$('#subtitle').html('test2'+'<span></span>');
+$('#subtitle').html('test3'+'<span></span>');
 var tes= ["</br>Introduction:<br/>"+"site.tagline "];
 const words = ["해적왕이야", "멋진 개발자가 될 거야"];
 const { text } = useTypewriter({
